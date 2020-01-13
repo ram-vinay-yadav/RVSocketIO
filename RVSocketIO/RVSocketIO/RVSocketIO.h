165dbd020932f9fb053faf6457f59c9b108508d7
Ram Vinay <ram.vinay@techugo.com>
2020-01-13 22:17:49 +0530

    Done

---
+++ RVSocketIO/RVSocketIO/RVSocketIO.h
@@ -0,0 +1,19 @@
+//
+//  RVSocketIO.h
+//  RVSocketIO
+//
+//  Created by Ram Vinay on 13/01/20.
+//  Copyright © 2020 Ram Vinay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RVSocketIO.
+FOUNDATION_EXPORT double RVSocketIOVersionNumber;
+
+//! Project version string for RVSocketIO.
+FOUNDATION_EXPORT const unsigned char RVSocketIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RVSocketIO/PublicHeader.h>
+
+
